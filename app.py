ab46ef5c2f8edbfbf00b5d033fe36f579bec3226
bhoobesh1 <bhoobeshmanickam@gmail.com>
2026-03-20 17:37:06 +0530

    port

--- app.py
+++ app.py
@@ -734,5 +734,5 @@ def internal_error(e):
 # ENTRY POINT  (dev only — use Gunicorn in prod)
 # ─────────────────────────────────────────
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8081))
     app.run(host="0.0.0.0", port=port, debug=False)
